ee7d7f0973f5768374c01bad1a1daa5c74616117
Janum Shah <jshah1006@gmail.com>
2021-04-25 22:06:19 -0400

    Submission

--- solutions.py
+++ solutions.py
@@ -1,7 +1,32 @@
 # Depth-first search
 def dfs(node, explored):
-    pass
+    if node not in explored:
+        explored.append(node)
+        for i in node.neighbors:
+            dfs(i, explored)
+    
+    return explored
+
 
 # Breadth-first search
 def bfs(start, goal):
-    pass
+    explored = []
+    queue = []
+
+    initial_path = []
+    initial_path.append(start)
+    queue.append(initial_path)
+
+    while (len(queue) > 0):
+        path = queue.pop(0)
+        temp_node = path[-1]
+        if temp_node not in explored:
+            for n in temp_node.neighbors:
+                new_path = path.copy()
+                new_path.append(n)
+                queue.append(new_path)
+                if n is goal:
+                    return new_path
+        explored.append(temp_node)
+
+
